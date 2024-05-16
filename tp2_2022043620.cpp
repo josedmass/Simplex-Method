99d219adf11096e500f1bcc464d925e4a0243607
josedmass <josedmassucato@gmail.com>
2024-05-16 17:01:11 -0300

    Implementação da lógica do Simplex
    1. Ótimo
    2. Ilimitado

--- tp2_2022043620.cpp
+++ tp2_2022043620.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <climits>
 #include <vector>
 
 #define INVIAVEL -1
@@ -8,7 +9,7 @@
 using namespace std;
 
 // Função auxiliar utilizada para depuração
-void print_tableau(vector<vector<int>>& tableau) {
+void print_tableau(const vector<vector<double>>& tableau) {
     for(int i = 0; i < tableau.size(); i++) {
         for(int j = 0; j < tableau[i].size(); j++) {
             cout << tableau[i][j] << " ";
@@ -19,14 +20,14 @@ void print_tableau(vector<vector<int>>& tableau) {
 }
 
 // Multiplica a matriz A pela matriz B
-vector<vector<int>> mult(vector<vector<int>>& A, vector<vector<int>>& B) {
+vector<vector<double>> mult(const vector<vector<double>>& A, const vector<vector<double>>& B) {
     int n1 = A.size(), m1 = A[0].size();
     int n2 = B.size(), m2 = B[0].size();
-    vector<vector<int>> C(n1, vector<int>(m2));
+    vector<vector<double>> C(n1, vector<double>(m2));
     if(m1 == n2) {
         for(int i = 0; i < n1; i++) {
             for(int j = 0; j < m2; j++) {
-                int res = 0;
+                double res = 0;
                 for(int k = 0; k < m1; k++)
                     res += A[i][k] * B[k][j];
                 C[i][j] = res;
@@ -36,10 +37,36 @@ vector<vector<int>> mult(vector<vector<int>>& A, vector<vector<int>>& B) {
     return C;
 }
 
+// Cria a matriz U, que soma lin1 multiplicada por val a lin2
+void matriz_pivoteamento(vector<vector<double>>& U, double val, int lin1, int lin2, int n) {
+    U = vector<vector<double>>(n+1, vector<double>(n+1));
+    for(int i = 0; i < n+1; i++) {
+        for(int j = 0; j < n+1; j++) {
+            if(i == j) U[i][j] = 1;
+            else if(i == lin2 && j == lin1) U[i][j] = val;
+            else U[i][j] = 0;
+        }
+    }
+}
+
+// Cria a matriz D, que multiplica a linha lin por um real val
+void matriz_multiplicacao(vector<vector<double>>& D, int lin, double val, int n) {
+    D = vector<vector<double>>(n+1, vector<double>(n+1));
+    for(int i = 0; i <= n; i++) {
+        for(int j = 0; j <= n; j++) {
+            if(i == j) {
+                if(i == lin) D[i][j] = val;
+                else D[i][j] = 1;
+            }
+            else D[i][j] = 0;
+        }
+    }
+}
+
 // Constrói o tableau inicial, seguindo a formatação do slide
-void tableau_inicial(vector<vector<int>>& tableau, vector<vector<int>>& A, vector<vector<int>>& b, vector<vector<int>>& c) {
+void tableau_inicial(vector<vector<double>>& tableau, const vector<vector<double>>& A, const vector<vector<double>>& b, const vector<vector<double>>& c) {
     int n = A.size(), m = A[0].size();
-    tableau = vector<vector<int>>(n+1, vector<int>(n+m+1));
+    tableau = vector<vector<double>>(n+1, vector<double>(n+m+1));
     for(int i = 0; i < n; i++) tableau[0][i] = 0;
     for(int i = n; i < n+m; i++) tableau[0][i] = -c[i-n][0];
     tableau[0][n+m] = 0;
@@ -68,8 +95,8 @@ void tableau_inicial(vector<vector<int>>& tableau, vector<vector<int>>& A, vecto
 }
 
 // Constrói o tableau auxiliar, quando não há uma base canônica trivial, seguindo a formatação do slide
-void aux_tableau_inicial(vector<vector<int>>& aux_tab, const vector<vector<int>>& tableau, int n, int m) {
-    aux_tab = vector<vector<int>>(n+1, vector<int>(n+m+n+1));
+void aux_tableau_inicial(vector<vector<double>>& aux_tab, const vector<vector<double>>& tableau, int n, int m) {
+    aux_tab = vector<vector<double>>(n+1, vector<double>(n+m+n+1));
     for(int i = 0; i < n; i++) aux_tab[0][i] = tableau[0][i];
     for(int i = n; i < n+m; i++) aux_tab[0][i] = 0;
     for(int i = n+m; i < n+m+n; i++) aux_tab[0][i] = 1;
@@ -98,7 +125,7 @@ void aux_tableau_inicial(vector<vector<int>>& aux_tab, const vector<vector<int>>
 }
 
 // Retorna um vetor com os índices da base canônica
-vector<int> canonico(vector<vector<int>>& tableau, int n, int m) {
+vector<int> idx_canonico(const vector<vector<double>>& tableau, int n, int m) {
     vector<int> ans, aux;
     for(int i = 1; i <= n; i++) {
         for(int j = n; j < n+m; j++) {
@@ -123,21 +150,85 @@ vector<int> canonico(vector<vector<int>>& tableau, int n, int m) {
     return ans;
 }
 
+// Faz com que a primeira linha das colunas canônicas seja zero
+void canonico(vector<vector<double>>& tableau, const vector<int>& can, int n) {
+    int it = 1;
+    for(int idx : can) {
+        //cout << "|" << idx << "|" << endl;
+        int val = -tableau[0][idx];
+        if(val == 0) continue;
+        vector<vector<double>> U;
+        matriz_pivoteamento(U, val, it, 0, n);
+        //print_tableau(tableau); cout << endl;
+        tableau = mult(U, tableau);
+        it++;
+    }
+    //print_tableau(tableau); cout << endl;
+}
+
 // Executa a primeira fase do simplex, verificando se a PL é viável
-pair<int, vector<vector<int>>> Fase1_Simplex(vector<vector<int>>& tableau, int n, int m) {
+pair<int, vector<vector<double>>> Fase1_Simplex(vector<vector<double>>& tableau, int n, int m) {
 
 }
 
 // Executa o simplex e retorna o tableau final
-pair<int, vector<vector<int>>> simplex(vector<vector<int>>& A, vector<vector<int>>& b, vector<vector<int>>& c) {
+pair<int, vector<vector<double>>> simplex(vector<vector<double>>& A, vector<vector<double>>& b, vector<vector<double>>& c) {
     int n = A.size(), m = A[0].size();
-    vector<vector<int>> tableau; 
+    vector<vector<double>> tableau; 
     tableau_inicial(tableau, A, b, c);
-    vector<int> can = canonico(tableau, n, m);
+    vector<int> can = idx_canonico(tableau, n, m);
+    
     if(can.size() == 0) { // Não há uma base canônica explícita, então buscamos resolver a PL auxiliar
-        vector<vector<int>> aux_tab;
+        vector<vector<double>> aux_tab;
         aux_tableau_inicial(aux_tab, tableau, n, m);
-        //pair<int, vector<vector<int>>> fase1 = Fase1_Simplex(aux_tab, n, m);
+        //pair<int, vector<vector<double>>> fase1 = Fase1_Simplex(aux_tab, n, m);
+    }
+    else {
+        // 1. Garantir que esteja na forma canônica
+        canonico(tableau, can, n);
+        // 2. Encontrar uma coluna na primeira linha de valor negativo
+        for(int i = n; i < n+m; i++) {
+            //print_tableau(tableau);
+            if(tableau[0][i] < 0) {
+                // 2.1. Buscamos a linha que tem a menor razão
+                int line = 0;
+                double razao = 100000000;
+                for(int j = 1; j <= n; j++) {
+                    if(tableau[j][i] > 0) {
+                        if((tableau[j][n+m]*1.0 / tableau[j][i]) < razao) {
+                            razao = tableau[j][n+m]*1.0 / tableau[j][i];
+                            line = j;
+                        }
+                    }
+                }
+                if(line == 0) return {ILIMITADO, tableau};
+                else {
+                    //cout << "|" << line << "|" << endl;
+                    if(tableau[line][i] != 1) {
+                        vector<vector<double>> D;
+                        matriz_multiplicacao(D, line, 1.0/tableau[line][i], n);
+                        // for(int it1 = 0; it1 <= n; it1++) {
+                        //     for(int it2 = 0; it2 <= n; it2++)
+                        //         cout << D[it1][it2] << " ";
+                        //     cout << endl;
+                        // }
+                        tableau = mult(D, tableau);
+                        // cout << "D" << endl;
+                        // print_tableau(tableau);
+                    }
+                    // Pivoteamos, fazendo com que todos os elementos além de line sejam 0
+                    for(int j = 0; j <= n; j++) {
+                        if(j == line) continue;
+                        vector<vector<double>> U;
+                        matriz_pivoteamento(U, -tableau[j][i], line, j, n);
+                        tableau = mult(U, tableau);
+                    }
+                    i = n;
+                }
+
+            }
+        }
+        return {OTIMO, tableau}; // Não tem mais nenhum valor negativo
     }
 
 }
@@ -147,9 +238,9 @@ int main() {
     int n, m;
     cin >> n >> m;
 
-    vector<vector<int>> A(n, vector<int>(m));
-    vector<vector<int>> b(n, vector<int>(1));
-    vector<vector<int>> c(m, vector<int>(1));
+    vector<vector<double>> A(n, vector<double>(m));
+    vector<vector<double>> b(n, vector<double>(1));
+    vector<vector<double>> c(m, vector<double>(1));
 
     // Passo 1: leitura dos dados
     for(int i = 0; i < m; i++) cin >> c[i][0];
@@ -160,12 +251,55 @@ int main() {
     }
 
     //Passo 2: execução do algoritmo simplex
-    // vector<vector<int>> tableau;
+    pair<int, vector<vector<double>>> simp = simplex(A, b, c);
+    if(simp.first == OTIMO) {
+        cout << "otima" << endl;
+        cout << simp.second[0][n+m] << endl; // valor ótimo
+        vector<int> can = idx_canonico(simp.second, n, m);
+        vector<int> x(m, 0);
+        int it = 1;
+        for(int idx : can) {
+            x[idx-n] = simp.second[it][n+m];
+            it++;
+        }
+        for(int val : x) cout << val << " "; // valores das variáveis ótimas
+        cout << endl;
+        for(int i = 0; i < n; i++) cout << simp.second[0][i] << " "; // certificado
+        cout << endl;
+    }
+    else if(simp.first == ILIMITADO) {
+        cout << "ilimitada" << endl;
+        vector<int> can = idx_canonico(simp.second, n, m);
+        vector<int> x(m, 0);
+        int it = 1;
+        for(int idx : can) {
+            x[idx-n] = simp.second[it][n+m];
+            it++;
+        }
+        for(int val : x) cout << val << " ";
+        cout << endl;
+        vector<int> d(m, 0);
+        int i = n;
+        for(; i < n+m; i++) {
+            if(simp.second[0][i] < 0) { // linha onde deu problema
+                d[i-n] = 1;
+                break;
+            }
+        }
+        it = 1;
+        for(int idx : can) {
+            d[idx - n] = -simp.second[it][i];
+            it++;
+        }
+        for(int val : d) cout << val << " ";
+        cout << endl;
+    }
+    // vector<vector<double>> tableau;
     // tableau_inicial(tableau, A, b, c);
-    // vector<vector<int>> aux;
-    // aux_tab_inicial(aux, tableau, n, m);
-    // print_tableau(tableau);
-    // print_tableau(aux);
+    // vector<vector<double>> aux;
+    // aux_tableau_inicial(aux, tableau, n, m);
+    // vector<int> can = idx_canonico(tableau, n, m);
+    // canonico(tableau, can, n);
 
     // vector<int> canonic = canonico(tableau, n, m);
     // for(int i : canonic) cout << i << " ";
